2818164dfe9a440a8f47fcc2fc79fbdc826f9221
DevSP <contatodevsp@gmail.com>
2021-03-13 17:37:51 -0300

    update: Estilizando Detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,65 @@
+import { useRouteMatch, Link } from 'react-router-dom'
+
+import { Header, Issues, RepositoryInfo } from './styles'
+
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg'
+
+interface RepositoryParams {
+    repository: string
+}
+
 const Repository: React.FC = () => {
+    const { params } = useRouteMatch<RepositoryParams>()
+
     return (
-        <h1>Repository</h1>
+        <>
+            <Header>
+                <img src={logoImg} alt="GitHub Explorer" />
+
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars0.githubusercontent.com/u/28929274?v=4" alt="Rocketseat" />
+
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Descrição do repositório</p>
+                    </div>
+                </header>
+
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="asdasd">
+                    <div>
+                        <strong>asdasdasd</strong>
+                        <p>asdasdasd</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
     )
 }
 
