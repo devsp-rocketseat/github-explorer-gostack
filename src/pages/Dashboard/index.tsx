4f61c1d4295010b60809d769db7c4a830033d113
DevSP <contatodevsp@gmail.com>
2021-03-13 16:50:51 -0300

    update: Salvando no Storage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react'
+import { useState, useEffect, FormEvent } from 'react'
 import api from '../../services/api'
 
 import { FiChevronRight } from 'react-icons/fi'
@@ -18,7 +18,18 @@ interface Repository {
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('')
     const [inputError, setInputError] = useState('')
-    const [repositories, setRepositories] = useState<Repository[]>([])
+
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepos = localStorage.getItem('repositories')
+
+        if (storagedRepos) return JSON.parse(storagedRepos)
+
+        return []
+    })
+
+    useEffect(() => {
+        localStorage.setItem('repositories', JSON.stringify(repositories))
+    }, [repositories])
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault()
