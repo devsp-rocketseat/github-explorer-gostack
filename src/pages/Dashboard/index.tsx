770ee52ea1d6cef6c044d26ed4c1fac742298d3b
DevSP <contatodevsp@gmail.com>
2021-03-13 16:23:10 -0300

    update: Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,52 +1,59 @@
+import { FormEvent, useState } from 'react'
+import api from '../../services/api'
+
 import { FiChevronRight } from 'react-icons/fi'
 import logoImg from '../../assets/logo.svg'
 
 import { Title, Form, Repositories } from './styles'
 
+interface Repository {
+    full_name: string
+    description: string
+    owner: {
+        login: string
+        avatar_url: string
+    }
+}
+
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('vitordevsp/casa-criativa')
+    const [repositories, setRepositories] = useState<Repository[]>([])
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault()
+
+        const response = await api.get<Repository>(`repos/${newRepo}`)
+
+        const repository = response.data
+
+        setRepositories([...repositories, repository])
+
+        setNewRepo('')
+    }
+
     return (
         <>
             <img src={logoImg} alt="GitHub Explorer" />
             <Title>Explore repositórios no GitHub</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório" />
+            <Form onSubmit={handleAddRepository}>
+                <input value={newRepo} onChange={e => setNewRepo(e.target.value)} placeholder="Digite o nome do repositório" />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
-                    <img src="https://github.com/vitordevsp.png" alt="Vitor DevSP" />
+                {repositories.map(repo => (
+                    <a key={repo.full_name} href="teste">
+                        <img src={repo.owner.avatar_url} alt={repo.owner.login} />
 
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-                    </div>
+                        <div>
+                            <strong>{repo.full_name}</strong>
+                            <p>{repo.description}</p>
+                        </div>
 
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="teste">
-                    <img src="https://github.com/vitordevsp.png" alt="Vitor DevSP" />
-
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="teste">
-                    <img src="https://github.com/vitordevsp.png" alt="Vitor DevSP" />
-
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Repositories>
         </>
     )
