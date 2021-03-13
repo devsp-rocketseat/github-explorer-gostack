165fd3adc77be53046122193a6c0bfcc6a656060
DevSP <contatodevsp@gmail.com>
2021-03-13 12:35:58 -0300

    update: Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,54 @@
-import { Title } from './styles'
+import { FiChevronRight } from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
     return (
-        <Title>Expore repositórios no GitHub</Title>
+        <>
+            <img src={logoImg} alt="GitHub Explorer" />
+            <Title>Explore repositórios no GitHub</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://github.com/vitordevsp.png" alt="Vitor DevSP" />
+
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img src="https://github.com/vitordevsp.png" alt="Vitor DevSP" />
+
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img src="https://github.com/vitordevsp.png" alt="Vitor DevSP" />
+
+                    <div>
+                        <strong>rocketseat/unform</strong>
+                        <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
     )
 }
 
